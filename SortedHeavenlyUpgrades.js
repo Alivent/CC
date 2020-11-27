36f388ac842d2a93ee5a3f7592987988bd0f9c38
Alivent <jerryremig@gmail.com>
2020-11-27 15:45:30 -0500

    Create SortedHeavenlyUpgrades.js

---
+++ SortedHeavenlyUpgrades.js
@@ -0,0 +1,118 @@
+if(assortPrestige = undefined)
+  var assortPrestige = 0;
+
+// Basic Heavenly Upgrades
+Game.PrestigeUpgrades[50] = assortPrestige++; // Legacy
+Game.PrestigeUpgrades[0]  = assortPrestige++; // Persistent Memory
+Game.PrestigeUpgrades[71] = assortPrestige++; // Heralds
+Game.PrestigeUpgrades[35] = assortPrestige++; // How to Bake Your Dragon
+Game.PrestigeUpgrades[83] = assortPrestige++; // Pet the Dragon
+
+// Quality of Life
+Game.PrestigeUpgrades[69] = assortPrestige++; // Inpired Checklist
+Game.PrestigeUpgrades[77] = assortPrestige++; // Genius Accounting
+Game.PrestigeUpgrades[70] = assortPrestige++; // Label Printer
+Game.PrestigeUpgrades[68] = assortPrestige++; // Eye of the Wrinkler
+
+// Customization
+Game.PrestigeUpgrades[39] = assortPrestige++; // Classic Dairy Selection
+Game.PrestigeUpgrades[40] = assortPrestige++; // Fanciful Dairy Selection
+Game.PrestigeUpgrades[49] = assortPrestige++; // Basic Wallpaper Assortment
+
+// Cookies and not Cookies
+Game.PrestigeUpgrades[56] = assortPrestige++; // Heavenly Cookies
+Game.PrestigeUpgrades[57] = assortPrestige++; // Wrinkly Cookies
+Game.PrestigeUpgrades[73] = assortPrestige++; // Sugar Crystal Cookies
+Game.PrestigeUpgrades[37] = assortPrestige++; // Tin of Butter Cookies
+Game.PrestigeUpgrades[2]  = assortPrestige++; // Tin of British Tea Biscuits
+Game.PrestigeUpgrades[3]  = assortPrestige++; // Box of Brand Biscuits
+Game.PrestigeUpgrades[4]  = assortPrestige++; // Box of Macarons
+Game.PrestigeUpgrades[75] = assortPrestige++; // Box of Not Cookies
+Game.PrestigeUpgrades[74] = assortPrestige++; // Box of Maybe Cookies
+Game.PrestigeUpgrades[76] = assortPrestige++; // Box of Pastries
+
+// Offline CPS and Synergies
+Game.PrestigeUpgrades[22] = assortPrestige++; // Twin Gates of Transcendence
+Game.PrestigeUpgrades[15] = assortPrestige++; // Angels
+Game.PrestigeUpgrades[16] = assortPrestige++; // Archangels
+Game.PrestigeUpgrades[17] = assortPrestige++; // Virtues
+Game.PrestigeUpgrades[18] = assortPrestige++; // Dominions
+Game.PrestigeUpgrades[19] = assortPrestige++; // Cherubim
+Game.PrestigeUpgrades[20] = assortPrestige++; // Seraphim
+Game.PrestigeUpgrades[21] = assortPrestige++; // God
+Game.PrestigeUpgrades[41] = assortPrestige++; // Belphegor
+Game.PrestigeUpgrades[42] = assortPrestige++; // Mammon
+Game.PrestigeUpgrades[43] = assortPrestige++; // Abaddon
+Game.PrestigeUpgrades[44] = assortPrestige++; // Satan
+Game.PrestigeUpgrades[45] = assortPrestige++; // Asmodeus
+Game.PrestigeUpgrades[46] = assortPrestige++; // Beelzebub
+Game.PrestigeUpgrades[47] = assortPrestige++; // Lucifer
+Game.PrestigeUpgrades[54] = assortPrestige++; // Synergies Vol. I
+Game.PrestigeUpgrades[55] = assortPrestige++; // Synergies Vol. II
+Game.PrestigeUpgrades[36] = assortPrestige++; // Chimera
+
+// Kittens and Milk
+Game.PrestigeUpgrades[32] = assortPrestige++; // Kitten Angels
+Game.PrestigeUpgrades[82] = assortPrestige++; // Kitten Wages
+Game.PrestigeUpgrades[84] = assortPrestige++; // Cat Ladies
+Game.PrestigeUpgrades[85] = assortPrestige++; // Milkhelp(R) lactose intolerance relief tablets
+
+// Cursors and Grandmas
+Game.PrestigeUpgrades[29] = assortPrestige++; // Starter Kit
+Game.PrestigeUpgrades[30] = assortPrestige++; // Starter Kitchen
+Game.PrestigeUpgrades[31] = assortPrestige++; // Halo Gloves
+Game.PrestigeUpgrades[86] = assortPrestige++; // Aura Gloves
+Game.PrestigeUpgrades[87] = assortPrestige++; // Luminous Gloves
+Game.PrestigeUpgrades[53] = assortPrestige++; // Five-Finger Discount
+
+// Golden Cookie Upgrades
+Game.PrestigeUpgrades[23] = assortPrestige++; // Heavenly Luck
+Game.PrestigeUpgrades[24] = assortPrestige++; // Lasting Forutne
+Game.PrestigeUpgrades[25] = assortPrestige++; // Decisive Fate
+Game.PrestigeUpgrades[38] = assortPrestige++; // Golden Switch
+Game.PrestigeUpgrades[48] = assortPrestige++; // Golden Cookie Alert Sound
+Game.PrestigeUpgrades[52] = assortPrestige++; // Residual Luck
+Game.PrestigeUpgrades[62] = assortPrestige++; // Lucky Digit
+Game.PrestigeUpgrades[63] = assortPrestige++; // Lucky Number
+Game.PrestigeUpgrades[64] = assortPrestige++; // Lucky Payout
+Game.PrestigeUpgrades[58] = assortPrestige++; // Distilled Essence of Redoubled Luck
+
+// Discounts
+Game.PrestigeUpgrades[26] = assortPrestige++; // Divine Discount
+Game.PrestigeUpgrades[27] = assortPrestige++; // Divine Sales
+Game.PrestigeUpgrades[28] = assortPrestige++; // Divine Bakeries
+
+// Seasonal Upgrades
+Game.PrestigeUpgrades[1]  = assortPrestige++; // Season Switcher
+Game.PrestigeUpgrades[10] = assortPrestige++; // Starspawn
+Game.PrestigeUpgrades[11] = assortPrestige++; // Starsnow
+Game.PrestigeUpgrades[12] = assortPrestige++; // Starterror
+Game.PrestigeUpgrades[13] = assortPrestige++; // Starlove
+Game.PrestigeUpgrades[14] = assortPrestige++; // Startrade
+Game.PrestigeUpgrades[72] = assortPrestige++; // Keepsakes
+
+// Wrinklers
+Game.PrestigeUpgrades[33] = assortPrestige++; // Unholy Bait
+Game.PrestigeUpgrades[34] = assortPrestige++; // Sacreligious Corruption
+Game.PrestigeUpgrades[51] = assortPrestige++; // Elder Spice
+
+// Sugar Lumps
+Game.PrestigeUpgrades[59] = assortPrestige++; // Stevia Caelestis
+Game.PrestigeUpgrades[60] = assortPrestige++; // Diabetica Daemonicus
+Game.PrestigeUpgrades[61] = assortPrestige++; // Sucralosia Inutilis
+Game.PrestigeUpgrades[65] = assortPrestige++; // Sugar Baking
+Game.PrestigeUpgrades[66] = assortPrestige++; // Sugar Craving
+Game.PrestigeUpgrades[67] = assortPrestige++; // Sugar Aging Process
+
+// Misc
+Game.PrestigeUpgrades[78] = assortPrestige++; // Shimmering Veil
+Game.PrestigeUpgrades[80] = assortPrestige++; // Reinforced Membrane
+Game.PrestigeUpgrades[79] = assortPrestige++; // Cosmic Beginner's Luck
+Game.PrestigeUpgrades[81] = assortPrestige++; // Fortune Cookies
+
+// Permenant Upgrade Slots
+Game.PrestigeUpgrades[5] = assortPrestige++; // Permenant Upgrade Slot I
+Game.PrestigeUpgrades[6] = assortPrestige++; // Permenant Upgrade Slot II
+Game.PrestigeUpgrades[7] = assortPrestige++; // Permenant Upgrade Slot III
+Game.PrestigeUpgrades[8] = assortPrestige++; // Permenant Upgrade Slot IV
+Game.PrestigeUpgrades[8] = assortPrestige++; // Permenant Upgrade Slot V
